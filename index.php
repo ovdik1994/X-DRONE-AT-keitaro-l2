02b2348fb178df8c89055146862c0c521fe6baba
Igor Ovdiienko <ovdienko1994@gmail.com>
2021-03-18 10:02:12 +0200

    first commit2

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Query params are:  
+
+require_once dirname(__FILE__) . '/kclient.php';
+$client = new KClient('https://babylight.info/api.php?', getenv('KEITARO_TOKEN'));
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
